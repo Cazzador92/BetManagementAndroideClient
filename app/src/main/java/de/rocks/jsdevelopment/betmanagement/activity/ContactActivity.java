146b70e1abc621e433039710b2d93cbbec36718c
Cazzador <cazzador92@gmail.com>
2015-02-01 18:38:11 +0100

    Begonnen Telefonkontakte als Teilnehmer zu nutzen.

---
+++ app/src/main/java/de/rocks/jsdevelopment/betmanagement/activity/ContactActivity.java
@@ -0,0 +1,10 @@
+package de.rocks.jsdevelopment.betmanagement.activity;
+
+/**
+ * Created by Cazza on 01.02.2015.
+ * infos.
+ * http://examples.javacodegeeks.com/android/core/provider/android-contacts-example/
+ * http://developer.android.com/training/contacts-provider/retrieve-names.html
+ */
+public class ContactActivity {
+}
---
+++ app/src/main/java/de/rocks/jsdevelopment/betmanagement/adapter/ContactListAdapter.java
@@ -0,0 +1,7 @@
+package de.rocks.jsdevelopment.betmanagement.adapter;
+
+/**
+ * Created by Sebastian on 01.02.2015.
+ */
+public class ContactListAdapter {
+}
